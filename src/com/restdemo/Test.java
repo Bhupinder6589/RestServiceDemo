ae493c31da8fd04fc8ff7b773a1d350a09ed3077
Bhupinder <bhupinder6589@gmail.com>
2022-11-06 19:16:13 +0530

    Initial_commit

---
+++ src/com/restdemo/Test.java
@@ -0,0 +1,22 @@
+package com.restdemo;
+import javax.ws.rs.GET;
+//http://localhost:8989/RestServiceDemo/rest/rs/show
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+
+
+
+@Path("/rs")
+public class Test {
+	@Path("/show")
+	@GET
+	@Produces(javax.ws.rs.core.MediaType.TEXT_HTML)
+	public String show()
+	{
+		return "hello World";
+	}
+	
+	
+	
+}
